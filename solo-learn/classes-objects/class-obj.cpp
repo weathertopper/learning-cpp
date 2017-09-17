bf86e0db98d2fa8f11510064b1547610116aab5d
weathertopper <nathanweatherly94@gmail.com>
2017-09-16 21:32:58 -0400

    starting objects

--- solo-learn/classes-objects/class-obj.cpp
+++ solo-learn/classes-objects/class-obj.cpp
@@ -3,8 +3,27 @@
 
 using namespace std;
 
+class BookCharacter
+{
+  private:
+    string name;
+  public:
+    BookCharacter(string _name){ //constructor
+      name = _name;
+    }
+    void setName(string _name)
+    {
+      name = _name;
+    }
+    string getName()
+    {
+      return name;
+    }
+};
+
 int main()
 {
-  cout << "Hello, World!" << endl;
+  BookCharacter tonks("Nymphadora Tonks");
+  cout << tonks.getName() << endl;
   return 0;
-}
+};
